6b5ae0655aed6ea31417777f5626271365822bb5
Navarro Guimarães <navasxll@hotmail.com>
2022-07-10 16:26:36 -0300

    Adding second exercise
    now the second exercise behaves like it should do

--- IF1015 - 1stActivity/IF1015 - 2ndExercise/client.js
+++ IF1015 - 1stActivity/IF1015 - 2ndExercise/client.js
@@ -4,21 +4,47 @@ const readLine = require('readline')
 
 const client = new net.Socket()
 
-const inputs = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+
+let username = "";
 
 const connectionCallback = () => {
-    console.log('conectado.')
+
+    const inputs = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })    
+
     inputs.addListener('line', line => {
-        client.write(line)
+        client.write(`${username}: ${line}`)
     })
-    client.write('Me conectei mwahaha')
+    client.write(`Nova conexão de ${username}`)
+
 }
 
-client.connect(4242, '127.0.0.1',connectionCallback)
+function askName(question){
+
+    const inputs = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    inputs.setPrompt(question);
+    inputs.prompt();
+
+    inputs.on('line', (userInput) => {
+        username = userInput.toString();
+        inputs.close();
+    });
+
+    inputs.on('close', () => {
+        console.log(`Muito prazer ${username}`);
+        client.connect(4242, '127.0.0.1',connectionCallback)
+    });
+
+}
 
 client.on("data", (data) => {
-    console.log(`Mensagem recebida do servidor: ${data}`)
+    console.log(data.toString())
 })
+
+askName("Antes de começarmos, nos diga seu nome: ")
--- IF1015 - 1stActivity/IF1015 - 2ndExercise/server.js
+++ IF1015 - 1stActivity/IF1015 - 2ndExercise/server.js
@@ -9,19 +9,28 @@ const inputs = readLine.createInterface({
 
 const connectedClients = []
 
-const handleConnection = socket => {
+const sendMessages = (senderPort, message) => {
 
-    console.log('O cliente foi conectado.')
+    const clientsToSendMessage = connectedClients.filter(socket => socket.remotePort != senderPort);
+    clientsToSendMessage.forEach(
+        socket => socket.write(message)
+    );
+
+}
+
+const handleConnection = socket => {
 
     socket.on('data', data => {
         
         const str = data.toString();
         if (str.toLowerCase() == 'end') { //apenas a comparação deve ser em lower case para exibir a mensagem conforme foi digitada
-            socket.write("você foi desconectado. Pressione CTRL + C para sair do cliente.")
-            socket.end()
+            socket.write("você foi desconectado. Pressione CTRL + C para sair do cliente.");
+            socket.end();
         }
-        console.log(`Mensagem recebida do Cliente: ${str}`)
 
+        console.log(str);
+        sendMessages(socket.remotePort, str);
+        
     })
 
 }
@@ -29,14 +38,14 @@ const handleConnection = socket => {
 const server = net.createServer(handleConnection)
 server.listen(4242, '127.0.0.1')
 
-server.on('connection', (client) => {
+server.on('connection', (socket) => {
+
+    socket.write("Olá, bem vinto ao chat na borda do espaço tempo. Aqui, nada tem passado, presente ou futuro.")
     
-    client.write("Oi Cliente! Bem vindo ao servidor. Mas nao te servirei dor, pode ficar tranquilo.")
-    
-    connectedClients.push(client)
+    connectedClients.push(socket);
     
     inputs.addListener('line', line => {
-        client.write(line)
+        socket.write(line)
     })
 
 })
